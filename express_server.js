4d8ccb5dedcbbffbd97a062708262cda69627522
Sayinthan Theivasothy <t.saj1994@gmail.com>
2020-03-16 23:56:43 +0000

    add form

--- express_server.js
+++ express_server.js
@@ -1,8 +1,20 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const PORT = 3000;
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+
+const generateRandomString = () => {
+  var result           = '';
+  var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  var charactersLength = characters.length;
+  for ( let i = 0; i < 6; i++ ) {
+     result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -14,12 +26,23 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars)
 })
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+})
+
 app.get("/urls/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL;
   let templateVars = {shortURL, LongURL: urlDatabase[shortURL]};
   res.render("urls_show", templateVars);
 })
 
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.send("OK");
+})
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+console.log(generateRandomString());
