02b82a8e293962c20171918f6a259f4f075248cd
Sayinthan Theivasothy <t.saj1994@gmail.com>
2020-03-17 21:58:53 +0000

    add login function

--- express_server.js
+++ express_server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cookieParser = require('cookie-parser');
 const app = express();
 const PORT = 3000;
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
 app.set("view engine", "ejs");
 
 const generateRandomString = () => {
@@ -14,61 +16,79 @@ const generateRandomString = () => {
      result += characters.charAt(Math.floor(Math.random() * charactersLength));
   }
   return result;
-}
+};
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
-}
+};
 
 app.get("/urls", (req, res) => {
-  let templateVars = {urls: urlDatabase};
+  let templateVars = {
+    username: req.cookies["username"],
+    urls: urlDatabase
+  };
   res.render("urls_index", templateVars)
-})
+});
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
-})
+  let templateVars = {
+    username: req.cookies["username"]
+  };
+  res.render("urls_new", templateVars);
+});
 
 app.get("/urls/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL;
-  let templateVars = {shortURL, LongURL: urlDatabase[shortURL]};
+  let templateVars = {
+    username: req.cookies["username"],
+    shortURL, LongURL: urlDatabase[shortURL]
+  };
   res.render("urls_show", templateVars);
-})
+});
 
 app.get("/u/:shortURL", (req, res) => {
   shortURL = req.params.shortURL;
   if(urlDatabase.hasOwnProperty(shortURL)) {
     let longURL = urlDatabase[shortURL];
     res.redirect(longURL);
-  }
-
-})
+  };
+});
 
 app.post("/urls", (req, res) => {
   const shortURL = generateRandomString();
   urlDatabase[shortURL] =  req.body.longURL;
   res.redirect(`urls/${shortURL}`);
-})
+});
 
 app.post("/urls/:shortURL/delete", (req, res) => {
   const shortURL = req.params.shortURL;
   if (urlDatabase.hasOwnProperty(shortURL)) {
     delete urlDatabase[shortURL];
     res.redirect("/urls");
-  }
-})
+  };
+});
 
 app.post("/urls/:shortURL/update", (req, res) => {
   const shortURL = req.params.shortURL;
   if (urlDatabase.hasOwnProperty(shortURL)) {
     res.redirect(`/urls/${shortURL}`);
-  }
-})
+  };
+});
 
 app.post("/urls/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL;
   urlDatabase[shortURL] = req.body.longURL;
+  res.redirect("/urls");
+})
+
+app.post("/login", (req, res) => {
+  res.cookie("username", req.body.username);
+  res.redirect("/urls");
+})
+
+app.post("/logout", (req, res) => {
+  res.clearCookie("username");
   res.redirect("/urls");
 })
 
